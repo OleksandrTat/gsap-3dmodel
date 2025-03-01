ac2a3943e6125ff675674e509a221480f014173f
OleksandrTat <ifpoleksandrtataryn@gmail.com>
2025-03-01 21:40:53 +0100

    другий блок

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 gsap.registerPlugin(ScrollTrigger)
 
+// section 1
 gsap.to(".leftfirst", {
     x: "-90%",
     rotate: "-15deg",
     ease: "power4.inOut",
     scrollTrigger: {
         trigger: ".section1",
-        start: "10% center",
+        start: "20% center",
         end: "55% center",
         scrub: true,
         markers: false,
@@ -18,7 +19,79 @@ gsap.to(".rightfirst", {
     ease: "power4.inOut",
     scrollTrigger: {
         trigger: ".section1",
-        start: "10% center",
+        start: "20% center",
+        end: "55% center",
+        scrub: true,
+        markers: false,
+    }
+})
+
+// section 2
+gsap.to(".section2first", {
+    opacity: 0,
+    ease: "power4.inOut",
+    scrollTrigger: {
+        trigger: ".section2",
+        start: "20% center",
+        end: "55% center",
+        scrub: true,
+        markers: false,
+    }
+})
+gsap.to(".left2-1", {
+    x: "-90%",
+    rotate: "-15deg",
+    ease: "power4.inOut",
+    scrollTrigger: {
+        trigger: ".section2",
+        start: "20% center",
+        end: "55% center",
+        scrub: true,
+        markers: false,
+    }
+})
+gsap.to(".right2-1", {
+    x: "90%",
+    rotate: "15deg",
+    ease: "power4.inOut",
+    scrollTrigger: {
+        trigger: ".section2",
+        start: "20% center",
+        end: "55% center",
+        scrub: true,
+        markers: false,
+    }
+})
+gsap.to(".section2second", {
+    opacity: 1,
+    ease: "power4.inOut",
+    scrollTrigger: {
+        trigger: ".section2",
+        start: "20% center",
+        end: "55% center",
+        scrub: true,
+        markers: false,
+    }
+})
+gsap.to(".left2-2", {
+    x: "-90%",
+    rotate: "-15deg",
+    ease: "power4.inOut",
+    scrollTrigger: {
+        trigger: ".section2",
+        start: "20% center",
+        end: "55% center",
+        scrub: true,
+        markers: false,
+    }
+})
+gsap.to(".right2-2", {
+    x: "90%",
+    rotate: "15deg",
+    ease: "power4.inOut",
+    scrollTrigger: {
+        trigger: ".section2",
+        start: "20% center",
         end: "55% center",
         scrub: true,
         markers: false,
