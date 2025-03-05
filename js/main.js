c8986b91b22c992b4e4718c77f5ae844f01bf1e5
Oleksandr <ifpoleksandrtataryn@gmail.com>
2025-03-05 12:47:16 +0100

    loader

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Cuando la página esté completamente cargada
+window.onload = function() {
+    // Ocultar el cargador
+    document.getElementById('loader').style.display = 'none';
+    // Mostrar el contenido de la página
+    document.getElementById('section').style.display = 'block';
+};
