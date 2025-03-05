85faf5c8640ce55a48c216f7ae9bae5e3f413918
Oleksandr <ifpoleksandrtataryn@gmail.com>
2025-03-05 10:46:23 +0100

    gsap

--- js/gsap.js
+++ js/gsap.js
@@ -1,193 +1,168 @@
-gsap.registerPlugin(ScrollTrigger)
+  gsap.registerPlugin(ScrollTrigger)
 
-// section 1
-gsap.to(".section1", {
-    // opacity: 1,
-    ease: "power2.inOut",
-    scrollTrigger: {
-        trigger: ".section1",
-        start: "20% center",
-        end: "55% center",
-        scrub: true,
-        markers: false,
-    }
-})
-gsap.to(".leftfirst", {
-    x: "-90%",
-    rotate: "-15deg",
-    ease: "power2.inOut",
-    scrollTrigger: {
-        trigger: ".section1",
-        start: "20% center",
-        end: "55% center",
-        scrub: true,
-        markers: false,
-    }
-})
-gsap.to(".rightfirst", {
-    x: "90%",
-    rotate: "15deg",
-    ease: "power2.inOut",
-    scrollTrigger: {
-        trigger: ".section1",
-        start: "20% center",
-        end: "55% center",
-        scrub: true,
-        markers: false,
-    }
-})
+  // section 1
+  gsap.to(".section1", {
+      // opacity: 1,
+      ease: "power2.inOut",
+      scrollTrigger: {
+          trigger: ".section1",
+          start: "20% center",
+          end: "55% center",
+          scrub: true,
+          markers: false,
+      }
+  })
+  gsap.to(".leftfirst", {
+      x: "-90%",
+      rotate: "-15deg",
+      ease: "power2.inOut",
+      scrollTrigger: {
+          trigger: ".section1",
+          start: "20% center",
+          end: "55% center",
+          scrub: true,
+          markers: false,
+      }
+  })
+  gsap.to(".rightfirst", {
+      x: "90%",
+      rotate: "15deg",
+      ease: "power2.inOut",
+      scrollTrigger: {
+          trigger: ".section1",
+          start: "20% center",
+          end: "55% center",
+          scrub: true,
+          markers: false,
+      }
+  })
 
-// section 2
-gsap.to(".section2first", {
-    opacity: 0,
-    ease: "power2.inOut",
-    scrollTrigger: {
-        trigger: ".section2",
-        start: "20% center",
-        end: "55% center",
-        scrub: true,
-        markers: false,
-    }
-})
-gsap.to(".left2-1", {
-    x: "-90%",
-    rotate: "-15deg",
-    ease: "power2.inOut",
-    scrollTrigger: {
-        trigger: ".section2",
-        start: "20% center",
-        end: "55% center",
-        scrub: true,
-        markers: false,
-    }
-})
-gsap.to(".right2-1", {
-    x: "90%",
-    rotate: "15deg",
-    ease: "power2.inOut",
-    scrollTrigger: {
-        trigger: ".section2",
-        start: "20% center",
-        end: "55% center",
-        scrub: true,
-        markers: false,
-    }
-})
-gsap.to(".section2second", {
-    opacity: 1,
-    ease: "power2.inOut",
-    scrollTrigger: {
-        trigger: ".section2",
-        start: "20% center",
-        end: "55% center",
-        scrub: true,
-        markers: false,
-    }
-})
-gsap.to(".left2-2", {
-    x: "-90%",
-    rotate: "-15deg",
-    ease: "power2.inOut",
-    scrollTrigger: {
-        trigger: ".section2",
-        start: "20% center",
-        end: "55% center",
-        scrub: true,
-        markers: false,
-    }
-})
-gsap.to(".right2-2", {
-    x: "90%",
-    rotate: "15deg",
-    ease: "power2.inOut",
-    scrollTrigger: {
-        trigger: ".section2",
-        start: "20% center",
-        end: "55% center",
-        scrub: true,
-        markers: false,
-    }
-})
+  // section 2
+  gsap.to(".section2first", {
+      opacity: 0,
+      ease: "power2.inOut",
+      scrollTrigger: {
+          trigger: ".section2",
+          start: "20% center",
+          end: "55% center",
+          scrub: true,
+          markers: false,
+      }
+  })
+  gsap.to(".left2-1", {
+      x: "-90%",
+      rotate: "-15deg",
+      ease: "power2.inOut",
+      scrollTrigger: {
+          trigger: ".section2",
+          start: "20% center",
+          end: "55% center",
+          scrub: true,
+          markers: false,
+      }
+  })
+  gsap.to(".right2-1", {
+      x: "90%",
+      rotate: "15deg",
+      ease: "power2.inOut",
+      scrollTrigger: {
+          trigger: ".section2",
+          start: "20% center",
+          end: "55% center",
+          scrub: true,
+          markers: false,
+      }
+  })
+  gsap.to(".section2second", {
+      opacity: 1,
+      ease: "power2.inOut",
+      scrollTrigger: {
+          trigger: ".section2",
+          start: "20% center",
+          end: "55% center",
+          scrub: true,
+          markers: false,
+      }
+  })
+  gsap.to(".left2-2", {
+      x: "-90%",
+      rotate: "-15deg",
+      ease: "power2.inOut",
+      scrollTrigger: {
+          trigger: ".section2",
+          start: "20% center",
+          end: "55% center",
+          scrub: true,
+          markers: false,
+      }
+  })
+  gsap.to(".right2-2", {
+      x: "90%",
+      rotate: "15deg",
+      ease: "power2.inOut",
+      scrollTrigger: {
+          trigger: ".section2",
+          start: "20% center",
+          end: "55% center",
+          scrub: true,
+          markers: false,
+      }
+  })
 
 
-// AN-225
-gsap.to(".text3", {
-    opacity: 1,
-    filter: "blur(0px)",
+  // AN-225
+  gsap.to(".text3", {
+      opacity: 1,
+      filter: "blur(0px)",
+      ease: "power2.inOut",
+      scrollTrigger: {
+          trigger: ".section3",
+          start: "0% center",
+          end: "center center",
+          scrub: true,
+          markers: false,
+      }
+  })
+
+
+  // AN-225 destroyed
+  gsap.to(".section4", {
+      // opacity: 1,
+      filter: "blur(0px)",
+      ease: "power2.inOut",
+      scrollTrigger: {
+          trigger: ".section4",
+          start: "0% center",
+          end: "center center",
+          scrub: true,
+          markers: false,
+      }
+  })
+
+
+  // Primera parte: de 0% a 48% (crecer el elemento)
+  gsap.to(".videobg", {
+    top: "0%",
+    left: "0%",
+    width: "100%",
+    height: "100%",
+    borderRadius: "0px",
     ease: "power2.inOut",
     scrollTrigger: {
-        trigger: ".section3",
-        start: "0% center",
+        trigger: ".section5",
+        start: "top center",
         end: "center center",
         scrub: true,
-        markers: false,
-    }
-})
-
-
-// AN-225 destroyed
-gsap.to(".section4", {
-    // opacity: 1,
-    filter: "blur(0px)",
-    ease: "power2.inOut",
-    scrollTrigger: {
-        trigger: ".section4",
-        start: "0% center",
-        end: "center center",
-        scrub: true,
-        markers: false,
-    }
-})
-
-
-
-
-
-// Конфігурація кольорів для кожної секції
-const colorStops = [
-    {section: ".section1", start: [1, 87, 183], end: [255, 215, 1]}, // Синьо-жовтий
-    {section: ".section2", start: [253, 1, 1], end: [1, 1, 1]},      // Червоно-чорний
-    {section: ".section3", start: [1, 87, 183], end: [255, 215, 1]}, // Синьо-жовтий
-    {section: ".section4", start: [253, 1, 1], end: [1, 1, 1]}       // Червоно-чорний
-  ];
-  
-  // Створюємо головну анімацію
-  const gradientAnimation = gsap.to(":root", {
-    duration: 1,
-    ease: "none",
-    paused: true,
-    modifiers: {
-      "--start-color": value => {
-        const progress = gradientAnimation.progress();
-        const current = Math.floor(progress * (colorStops.length - 1));
-        const next = Math.min(current + 1, colorStops.length - 1);
-        const ratio = (progress - current/(colorStops.length-1)) * (colorStops.length-1);
-        
-        const r = gsap.utils.interpolate(
-          colorStops[current].start[0], 
-          colorStops[next].start[0], 
-          ratio
-        );
-        // Аналогічно для G і B
-        return `rgb(${Math.round(r)},${Math.round(g)},${Math.round(b)})`;
-      },
-      // Аналогічний модифікатор для --end-color
+        toggleActions: "play reverse play reverse",
     }
   });
-  
-  // Налаштування скролу
-  ScrollTrigger.create({
-    trigger: "body",
-    start: "top top",
-    end: "max",
-    scrub: 1.5,
-    animation: gradientAnimation,
-    onUpdate: self => {
-      const sections = gsap.utils.toArray("section");
-      const progress = gsap.utils.normalize(0, sections.length - 1, self.progress);
-      gradientAnimation.progress(progress);
-    }
-  });
+
+
+
+
+
+
+
 
 
 
@@ -242,3 +217,41 @@ const colorStops = [
       }
     });
   });
+
+
+
+
+
+
+  gsap.registerPlugin(TextPlugin, EasePack);
+
+  let container = $("#demo"),
+      _sentenceEndExp = /(\.|\?|!)$/g;
+
+  function machineGun(text) {    
+    let words = text.split(" "),
+        tl = gsap.timeline({delay: 0.6, repeat: -1, repeatDelay: 2}),
+        wordCount = words.length,
+        time = 0,
+        word, element, duration, isSentenceEnd, i;
+    
+    for(i = 0; i < wordCount; i++){
+      word = words[i];
+      isSentenceEnd = _sentenceEndExp.test(word);
+      element = $("<h3>" + word + "</h3>").appendTo(container);
+      duration = Math.max(0.5, word.length * 0.08);
+      if (isSentenceEnd) {
+        duration += 0.4;
+      }
+      gsap.set(element, {autoAlpha:0, scale:0, z:0.01});
+      tl.to(element, duration, {scale:1.2,  ease:"slow(0.25, 0.9)"}, time)
+        .to(element, duration, {autoAlpha:1, ease:"slow(0.25, 0.9, true)"}, time);
+      time += duration - 0.05;
+      if (isSentenceEnd) {
+        time += 0.6;
+      }
+    }
+    
+  }
+
+  machineGun("Glory to Ukraine! Glory to the heroes!");
